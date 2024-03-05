2053d8b9a37a50674dda9e0f64d7c9fb1218f819
m-jawa-d <chudhryjawad@gmail.com>
2024-03-05 11:50:19 +0500

    functionality updates

--- popup.js
+++ popup.js
@@ -1,15 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
     var checkbox = document.getElementById('invertCheckbox');
 
+    // Check and apply the stored checkbox state from local storage
+    var isChecked = localStorage.getItem('invertCheckboxState') === 'true';
+    checkbox.checked = isChecked;
+    applyPageColorBasedOnCheckbox(isChecked);
+
     checkbox.addEventListener('change', function () {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                func: checkbox.checked ? invertPageColors : resetPageColors
-            });
-        });
+        // Save the checkbox state to local storage
+        localStorage.setItem('invertCheckboxState', checkbox.checked);
+        applyPageColorBasedOnCheckbox(checkbox.checked);
     });
 });
+
+function applyPageColorBasedOnCheckbox(isChecked) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            func: isChecked ? invertPageColors : resetPageColors
+        });
+    });
+}
 
 function invertPageColors() {
     document.documentElement.style.filter = 'invert(1)';
